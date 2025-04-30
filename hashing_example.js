eb33d11cbef2f756b4842a72bd05663adee4b153
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-30 12:24:41 -0400

    Initialize hashing project files

---
+++ hashing_example.js
@@ -0,0 +1,26 @@
+// import hashing_utils from same-level dir
+const utils = require('./hashing_utils');
+
+// TODO: setup the hashing dynamic-arrays
+const serverSet1 = [
+    'server0',
+    'server1',
+    'server2',
+    'server3',
+    'server4',
+    'server5'
+];
+
+//
+const serverSet2 = [
+    'server0',
+    'server1',
+    'server2',
+    'server3',
+    'server4',
+];
+
+//
+const usernames = [
+
+];
